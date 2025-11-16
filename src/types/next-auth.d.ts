33344fbd33d33e557bef739ea4f3f280f4d4475d
gauravwaghmare23 <gauravwaghmare95032@gmail.com>
2025-11-16 19:15:37 +0530

    implemented authentication with user verification and JWT support using NextAuth

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,32 @@
+import "next-auth";
+import 'next-auth/jwt';
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessage?: boolean;
+    username?: string;
+    email?: string;
+    }
+    interface Session {
+      user: {
+        _id?: string;
+        isVerified?: boolean;
+        isAcceptingMessage?: boolean;
+        username?: string;
+        email?: string;
+      } & DefaultSession["user"];
+    }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessage?: boolean;
+    username?: string;
+    email?: string;
+  }
+}
